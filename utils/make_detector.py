d9b45284a4812e9d687a87b1d180b5859a5f4fb8
N. Duane Loh <duaneloh@gmail.com>
2016-04-09 17:31:03 +0800

    Clarified polarization notation

--- utils/make_detector.py
+++ utils/make_detector.py
@@ -29,14 +29,14 @@ if __name__ == "__main__":
         qscaling    = 1. / pm['wavelength'] / q_pm['q_sep']
         (x, y)      = np.mgrid[0:pm['dets_x'], 0:pm['dets_y']]
         (x, y)      = (x.flatten()-det_cen_x, y.flatten()-det_cen_y)
-        (qx, qy)    = (pm['pixsize']*x, pm['pixsize']*y)
-        norm        = np.sqrt(qx*qx + qy*qy + pm['detd']*pm['detd'])
-        (qx, qy)    = (qx*qscaling/norm, qy*qscaling/norm)
+        (px, py)    = (pm['pixsize']*x, pm['pixsize']*y)
+        norm        = np.sqrt(px*px + py*py + pm['detd']*pm['detd'])
+        polar       = read_config.compute_polarization(pm['polarization'], px, py, norm)
+        (qx, qy)    = (px*qscaling/norm, py*qscaling/norm)
         qz          = qscaling*(pm['detd']/norm - 1.)
         qx         *= pm['qscale'] * pm['pixsize'] / pm['detd']
         qy         *= pm['qscale'] * pm['pixsize'] / pm['detd']
         qz         *= pm['qscale'] * pm['pixsize'] / pm['detd']
-        polar       = read_config.compute_polarization(pm['polarization'], qx, qy, norm)
         logging.info('{:<15}:{:10.4f}'.format('qmax', np.sqrt(qx*qx + qy*qy + qz*qz).max()))
         solid_angle = pm['detd']*(pm['pixsize']*pm['pixsize']) / np.power(norm, 3.0)
         solid_angle = polar*solid_angle
