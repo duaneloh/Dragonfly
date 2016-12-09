7f059b2a0b127e284f330fe9ec510cc7e44f6d5d
Kartik Ayyer <kartik.ayyer@desy.de>
2016-12-09 17:13:34 +0100

    psanatodet: Create detector file from PSANA detector interface (needs some testing)

---
+++ utils/convert/psanatodet.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python
+
+'''
+Convert PSANA geometry file to detector file
+
+Needs:
+    Experiment string (eg. exp=cxim7816:run=10)
+    Detector name (eg. DsaCsPad)
+
+Produces:
+    Detector file in output_folder
+'''
+
+import os
+import numpy as np
+import h5py
+import sys
+import logging
+#Add utils directory to pythonpath
+sys.path.append(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
+from py_src import py_utils
+from py_src import writeemc
+from py_src import read_config
+
+try:
+    from psana import *
+except ImportError:
+    print 'This utility needs PSANA to run. Unable to import psana module'
+    sys.exit(1)
+
+if __name__ == '__main__':
+    logging.basicConfig(filename='recon.log', level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+    parser      = py_utils.my_argparser(description='cheetahtodet')
+    parser.add_argument('exp_string', help='PSANA experiment string')
+    parser.add_argument('det_name', help='Detector name (either source string or alias)')
+    args        = parser.special_parse_args()
+
+    logging.info('Starting psanatodet...')
+    logging.info(' '.join(sys.argv))
+    pm          = read_config.get_detector_config(args.config_file, show=args.vb)
+    q_pm        = read_config.compute_q_params(pm['detd'], pm['dets_x'], pm['dets_y'], pm['pixsize'], pm['wavelength'], pm['ewald_rad'], show=args.vb)
+    output_folder = read_config.get_filename(args.config_file, 'emc', 'output_folder')
+
+    ds = psana.DataSource(args.exp_string + ':idx')
+    run = ds.runs().next()
+    times = run.times()
+    evt = run.event(times[0])
+    det = psana.Detector(args.det_name)
+
+    detd = pm['detd']
+    cx = det.coords_x(evt).flatten() * 1.e-3
+    cy = det.coords_y(evt).flatten() * 1.e-3
+    rad = np.sqrt(cx*cx + cy*cy)
+    norm = np.sqrt(cx*cx + cy*cy + detd*detd)
+    polar = read_config.compute_polarization(pm['polarization'], cx, cy, norm)
+    qx = cx * qscaling / norm
+    qy = cy * qscaling / norm
+    qz = qscaling * (detd / norm - 1.)
+    solid_angle = pm['detd']*(pm['pixsize']*pm['pixsize']) / np.power(norm, 3.0)
+    solid_angle = polar*solid_angle
+
+    mask = det.mask(evt, status=True, calib=True).flatten()
+    mask = 1 - mask
+    mask[mask==1] = 2
+    rmax = min(x.max(), np.abs(x.min()), y.max(), np.abs(y.min()))
+    mask = np.zeros(solid_angle.shape, dtype='u1')
+    mask[(rad>rmax) & (mask==0)] = 1
+
+    det_file = output_folder + '/' + args.exp_string.split(':')[0][4:] + '.dat'
+    print 'Writing detector file to', det_file
+    
+    with open(det_file, "w") as fp:
+        fp.write(str(qx.shape[0]) + "\n")
+        for t0,t1,t2,t3,t4 in zip(qx,qy,qz,solid_angle,mask):
+            txt = "%21.15e %21.15e %21.15e %21.15e %d\n"%(t0, t1, t2, t3, t4)
+            fp.write(txt)
