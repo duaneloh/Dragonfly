59969775a7b0b0351ae3cff5b6cae7c39f91990e
Kartik Ayyer <kartik.ayyer@desy.de>
2017-10-06 22:31:57 +0200

    detector.c: Can now parse new type detector file which has detd/pixsize and ewald_rad in the first line

--- src/detector.c
+++ src/detector.c
@@ -1,17 +1,21 @@
 #include "detector.h"
 
-int parse_detector(char *fname, struct detector *det, int norm_flag) {
+double parse_detector(char *fname, struct detector *det, int norm_flag) {
 	int t, d ;
-	double mean_pol = 0. ;
+	double q, qmax = -1., mean_pol = 0. ;
+	char line[1024] ;
 	
 	det->rel_num_pix = 0 ;
+	det->detd = 0. ;
+	det->ewald_rad = 0. ;
 	
 	FILE *fp = fopen(fname, "r") ;
 	if (fp == NULL) {
 		fprintf(stderr, "det_fname %s not found. Exiting...1\n", fname) ;
-		return 1 ;
+		return -1. ;
 	}
-	fscanf(fp, "%d", &det->num_pix) ;
+	fgets(line, 1024, fp) ;
+	sscanf(line, "%d %lf %lf\n", &det->num_pix, &det->detd, &det->ewald_rad) ;
 	det->pixels = malloc(4 * det->num_pix * sizeof(double)) ;
 	det->mask = malloc(det->num_pix * sizeof(uint8_t)) ;
 	for (t = 0 ; t < det->num_pix ; ++t) {
@@ -20,6 +24,9 @@ int parse_detector(char *fname, struct detector *det, int norm_flag) {
 		fscanf(fp, "%" SCNu8, &det->mask[t]) ;
 		if (det->mask[t] < 1)
 			det->rel_num_pix++ ;
+		q = pow(det->pixels[t*4+0], 2.) + pow(det->pixels[t*4+1], 2.) + pow(det->pixels[t*4+2], 2.) ;
+		if (q > qmax)
+			qmax = q ;
 		mean_pol += det->pixels[t*4 + 3] ;
 	}
 	
@@ -31,7 +38,7 @@ int parse_detector(char *fname, struct detector *det, int norm_flag) {
 	
 	fclose(fp) ;
 	
-	return 0 ;
+	return sqrt(qmax) ;
 }
 
 void free_detector(struct detector *det) {
--- src/detector.h
+++ src/detector.h
@@ -5,14 +5,16 @@
 #include <stdlib.h>
 #include <inttypes.h>
 #include <stdint.h>
+#include <math.h>
 
 struct detector {
 	int num_pix, rel_num_pix ;
+	double detd, ewald_rad ;
 	double *pixels ;
 	uint8_t *mask ;
 } ;
 
-int parse_detector(char*, struct detector*, int) ;
+double parse_detector(char*, struct detector*, int) ;
 void free_detector(struct detector*) ;
 
 #endif //DETECTOR_H
