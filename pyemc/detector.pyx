4b3195f44f63414848482de0af66a8592b2f3489
Kartik Ayyer <kartik.ayyer@desy.de>
2018-01-23 10:08:16 +0100

    pyemc: Wrapped generate_detectors, but without support for multiple detectors

--- pyemc/detector.pyx
+++ pyemc/detector.pyx
@@ -1,6 +1,7 @@
 import numpy as np
 cimport numpy as np
 from cpython.mem cimport PyMem_Malloc, PyMem_Free
+from libc.stdio cimport FILE, fdopen
 cimport emc 
 cimport openmp
 from detector cimport detector
@@ -8,6 +9,16 @@ from detector cimport detector
 cdef class detector:
 	def __init__(self):
 		self.det = <emc.detector*> PyMem_Malloc(sizeof(emc.detector))
+
+	def generate_detectors(self, config_file, norm_flag=True, config_section='emc', rank=0, num_proc=1):
+		if emc.config_section[0] == '\0':
+			emc.config_section[:len(config_section)] = config_section
+			emc.config_section[len(config_section)] = '\0'
+		if emc.rank == 0: emc.rank = rank
+		if emc.num_proc == 0: emc.num_proc = num_proc
+		
+		cdef FILE* config_fp = fdopen(config_file.fileno(), 'r')
+		emc.generate_detectors(config_fp, &self.det, int(norm_flag))
 
 	def parse_detector(self, fname, norm_flag=True):
 		cdef char* c_fname = fname
