643e1bbea657221282b302dd81acea67ba1dbb2b
Kartik Ayyer <kartik.ayyer@desy.de>
2018-01-23 09:06:47 +0100

    pyemc: Removed unnecessary setter in quat

--- pyemc/quat.pyx
+++ pyemc/quat.pyx
@@ -8,7 +8,7 @@ from quat cimport rotation
 cdef class rotation:
 	def __init__(self):
 		self.rot = <emc.rotation*> PyMem_Malloc(sizeof(emc.rotation))
-		self.icosahedral_flag = 0
+		self.rot.icosahedral_flag = 0
 
 	def generate_quaternion(self, config_file, config_section='emc', rank=0, num_proc=1):
 		if emc.config_section[0] == '\0':
@@ -40,6 +40,3 @@ cdef class rotation:
 	def quat(self): return np.asarray(<double[:self.num_rot*5]> self.rot.quat).reshape(-1,5)
 	@property
 	def icosahedral_flag(self): return bool(self.rot.icosahedral_flag)
-	@icosahedral_flag.setter
-	def icosahedral_flag(self, value):
-		self.rot.icosahedral_flag = value
