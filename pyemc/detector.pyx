7d76ed35a08d0dee9c7315247a3e48a75b4cd6b4
Kartik Ayyer <kartik.ayyer@desy.de>
2018-01-29 15:44:58 +0100

    pyemc: detector: Catching NULL struct case. Fixed reinitialize bug in parse_detector. Fixed mapping array initialization

--- pyemc/detector.pyx
+++ pyemc/detector.pyx
@@ -1,5 +1,6 @@
 import numpy as np
 cimport numpy as np
+import sys
 from cpython.mem cimport PyMem_Malloc, PyMem_Free
 cimport emc 
 cimport openmp
@@ -10,6 +11,7 @@ cdef class detector:
 		self.det = <emc.detector*> PyMem_Malloc(sizeof(emc.detector))
 		self.det.num_det = 0
 		self.det.num_dfiles = 0
+		self.det.mapping = [0]*1024
 
 	def generate_detectors(self, config_fname, norm_flag=True, config_section='emc'):
 		cdef char* c_config_fname = config_fname
@@ -29,6 +31,7 @@ cdef class detector:
 	def parse_detector(self, fname, norm_flag=True):
 		cdef char* c_fname = fname
 		if self.det.num_det > 0: self.free_detector()
+		self.__init__()
 		self.det.num_det = 1
 		self.det.num_dfiles = 0
 		qmax = emc.parse_detector(c_fname, self.det, int(norm_flag))
@@ -37,27 +40,29 @@ cdef class detector:
 
 	def free_detector(self):
 		emc.free_detector(self.det)
+		self.det = NULL
+
+	def __del__(self):
+		self.free_detector()
 
 	@property
-	def num_pix(self): return self.det.num_pix
+	def num_pix(self): return self.det.num_pix if self.det != NULL else None
 	@property
-	def rel_num_pix(self): return self.det.rel_num_pix
+	def rel_num_pix(self): return self.det.rel_num_pix if self.det != NULL else None
 	@property
-	def detd(self): return self.det.detd
+	def detd(self): return self.det.detd if self.det != NULL else None
 	@property
-	def ewald_rad(self): return self.det.ewald_rad
+	def ewald_rad(self): return self.det.ewald_rad if self.det != NULL else None
 	@property
-	def pixels(self): return np.asarray(<double[:4*self.num_pix]>self.det.pixels).reshape(-1,4)
+	def pixels(self): return np.asarray(<double[:4*self.num_pix]>self.det.pixels).reshape(-1,4) if self.det != NULL else None
 	@property
-	def mask(self): return np.asarray(<emc.uint8_t[:self.num_pix]>self.det.mask)
+	def mask(self): return np.asarray(<emc.uint8_t[:self.num_pix]>self.det.mask) if self.det != NULL else None
 
 	# Only relevant for first detector in list
 	@property
-	def num_det(self): return self.det.num_det
+	def num_det(self): return self.det.num_det if self.det != NULL else None
 	@property
-	def num_dfiles(self): return self.det.num_dfiles
+	def num_dfiles(self): return self.det.num_dfiles if self.det != NULL else None
 	@property
-	def mapping(self): return np.asarray(self.det.mapping, dtype='i4')
-
-
+	def mapping(self): return np.asarray(self.det.mapping, dtype='i4') if self.det != NULL else None
 
