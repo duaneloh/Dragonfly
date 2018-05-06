bef8818c79eccbe431bcf9af0b072162d28f67d1
Kartik Ayyer <kartik.ayyer@desy.de>
2018-05-06 09:11:45 +0200

    frame_panel: Showing masked part of frame in gray

--- utils/py_src/frame_panel.py
+++ utils/py_src/frame_panel.py
@@ -118,6 +118,7 @@ class Frame_panel(QtWidgets.QWidget):
             s = self.plot_slice(num)
         else:
             s = self.fig.add_subplot(111)
+        s.set_facecolor('gray')
         s.imshow(frame, vmin=0, vmax=float(self.rangestr.text()), interpolation='none', cmap=self.parent.cmap)
         title = '%d photons' % frame.sum()
         if frame is None and (mode == 1 or mode == 3):
