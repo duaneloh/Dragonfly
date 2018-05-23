26553cc052a916ee9599499bfba20f8e08bfd83b
Benedikt J. Daurer <bjdaurer@gmail.com>
2018-05-23 11:55:01 +0800

    Clean up

--- utils/convert/condortoemc.py
+++ utils/convert/condortoemc.py
@@ -25,9 +25,6 @@ if __name__ == '__main__':
     logging.basicConfig(filename='recon.log', level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
     parser      = py_utils.my_argparser(description='condortoemc')
     parser.add_argument('condor_fname', help='Condor CXI file to convert to emc format')
-    #parser.add_argument('-d', '--dset_name', help='Name of HDF5 dataset containing photon data', default=None)
-    #parser.add_argument('-s', '--sel_file', help='Path to text file containing indices of frames or a set of 0 or 1 values. Default: Do all', default=None)
-    #parser.add_argument('-S', '--sel_dset', help='Same as --sel_file, but pointing to the name of an HDF5 dataset', default=None)
     parser.add_argument('-l', '--list', help='condor_fname is list of condor CXI files rather than a single one', action='store_true', default=False)
     args        = parser.special_parse_args()
 
