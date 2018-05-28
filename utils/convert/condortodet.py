Correct scaling of q values for condor based detector file

utils/convert/condortodet.py
@@ -74,11 +74,11 @@ if __name__ == '__main__':
 
     # DRAGONFLY CALCULATION OF QMAP AND SOLID ANGLE
     # ---------------------------------------------
-    #q_pm = read_config.compute_q_params(pm['detd'], pm['dets_x'], pm['dets_y'], pm['pixsize'], pm['wavelength'], pm['ewald_rad'], show=args.vb)
-    #fov_len     = 2 * int(np.ceil(q_pm['fov_in_A']/q_pm['half_p_res']/2.)) + 3
-    #det_cen_x   = pm['detc_x']
-    #det_cen_y   = pm['detc_y']
-    #qscaling    = 1. / pm['wavelength'] / q_pm['q_sep']
+    q_pm = read_config.compute_q_params(pm['detd'], pm['dets_x'], pm['dets_y'], pm['pixsize'], pm['wavelength'], pm['ewald_rad'], show=args.vb)
+    fov_len     = 2 * int(np.ceil(q_pm['fov_in_A']/q_pm['half_p_res']/2.)) + 3
+    det_cen_x   = pm['detc_x']
+    det_cen_y   = pm['detc_y']
+    qscaling    = 1. / pm['wavelength'] / q_pm['q_sep']
     #(x, y)      = np.mgrid[0:pm['dets_x'], 0:pm['dets_y']]
     #print(x - det_cen_x), det_cen_x
     #(x, y)      = (x.flatten()-det_cen_x, y.flatten()-det_cen_y)
@@ -89,6 +89,9 @@ if __name__ == '__main__':
     #qz          = qscaling*(pm['detd']/norm - 1.)
     #solid_angle = pm['detd']*(pm['pixsize']*pm['pixsize']) / np.power(norm, 3.0)
     #solid_angle = polar*solid_angle
+    qx = condor_qx / (2 * np.pi) * condor_wavelength * qscaling
+    qy = condor_qy / (2 * np.pi) * condor_wavelength * qscaling
+    qz = condor_qz / (2 * np.pi) * condor_wavelength * qscaling
 
     if pm['mask_fname'] is None:
         mask = (condor_mask == 0).astype(np.int)
@@ -103,6 +106,6 @@ if __name__ == '__main__':
     
     with open(det_file, "w") as fp:
         fp.write(str(condor_qx.shape[0]) + "\n")
-        for t0,t1,t2,t3,t4 in zip(condor_qx,condor_qy,condor_qz,condor_solid_angle,mask):
+        for t0,t1,t2,t3,t4 in zip(qx,qy,qz,condor_solid_angle,mask):
             txt = "%21.15e %21.15e %21.15e %21.15e %d\n"%(t0, t1, t2, t3, t4)
             fp.write(txt)