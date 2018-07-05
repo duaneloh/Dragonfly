ebfdaf612cb544d1d8b9f81fcdbebb242d98de17
Kartik Ayyer <kartik.ayyer@desy.de>
2018-07-05 11:16:02 +0200

    emc: Added commented-out refinement function

--- src/max_emc.c
+++ src/max_emc.c
@@ -10,12 +10,15 @@
 #include "emc.h"
 
 struct max_data {
-	int within_openmp ;
+	int refinement, within_openmp ;
+	
 	// Common only
 	double *max_exp, *u, **probab ;
+	
 	// Private only
 	double *model, *weight, *scale ;
-	double **view ;
+	double **all_views ;
+	
 	// Both
 	double *max_exp_p, *p_sum ;
 	double *info, *likelihood ;
@@ -30,6 +33,7 @@ static void calculate_prob(int, struct max_data*, struct max_data*) ;
 static void normalize_prob(struct max_data*, struct max_data*) ;
 static void update_tomogram(int, struct max_data*, struct max_data*) ;
 static void merge_tomogram(int, struct max_data*) ;
+//static void refine_frame(int, struct dataset*, struct max_data*, struct max_data*) ;
 static void combine_information_omp(struct max_data*, struct max_data*) ;
 static double combine_information_mpi(struct max_data*) ;
 static void save_output(struct max_data*) ;
@@ -41,6 +45,7 @@ double maximize() {
 	gettimeofday(&tm1, NULL) ;
 	iter->mutual_info = 0. ;
 	struct max_data *common_data = malloc(sizeof(struct max_data)) ;
+	common_data->refinement = 0 ;
 	common_data->within_openmp = 0 ;
 	
 	allocate_memory(common_data) ;
@@ -51,6 +56,7 @@ double maximize() {
 		int r ;
 		struct max_data *priv_data = malloc(sizeof(struct max_data)) ;
 		
+		priv_data->refinement = 0 ;
 		priv_data->within_openmp = 1 ;
 		allocate_memory(priv_data) ;
 		
@@ -84,39 +90,158 @@ double maximize() {
 	return avg_likelihood ;
 }
 
+/*
+double refine_maximize() {
+	double avg_likelihood ;
+	struct max_data *common_data = malloc(sizeof(struct max_data)) ;
+	
+	gettimeofday(&tm1, NULL) ;
+	iter->mutual_info = 0. ;
+	common_data->refinement = 1 ;
+	common_data->within_openmp = 0 ;
+	allocate_memory(common_data) ;
+	
+	iter->rescale = calculate_rescale(common_data) ;
+	
+	#pragma omp parallel default(shared)
+	{
+		int d ;
+		struct dataset *curr = frames ;
+		struct max_data *priv_data = malloc(sizeof(struct max_data)) ;
+		
+		priv_data->refinement = 1 ;
+		priv_data->within_openmp = 1 ;
+		allocate_memory(priv_data) ;
+		
+		while (curr != NULL) {
+			#pragma omp for schedule(guided)
+			for (d = 0 ; d < curr->num_data ; ++d) {
+				if (frames->blacklist[curr->num_data_prev+d])
+					continue ;
+				refine_frame(d, curr, priv_data, common_data) ;
+			}
+			curr = curr->next ;
+		}
+		
+		combine_information_omp(priv_data, common_data) ;
+		free_memory(priv_data) ;
+	}
+	
+	avg_likelihood = combine_information_mpi(common_data) ;
+	if (!param->rank)
+		save_output(common_data) ;
+	free_memory(common_data) ;
+	
+	return avg_likelihood ;
+}
+
+void refine_frame(int d, struct dataset *curr, struct max_data *priv, struct max_data *common) {
+	int r, t, pixel ;
+	double max_exp = -DBL_MAX ;
+	double p_sum = 0. ;
+	double new_scale = 0. ;
+	double *prob = priv->probab[0] ;
+	
+	for (r = 0 ; r < num_rot_sub[d] ; ++r) {
+		(*slice_gen)(&quat->quat[quat_sub[d][r]*5], 1., view, iter->model1, iter->size, det) ;
+		
+		prob[r] = common->u[quat_sub[d][r]] * iter->scale[curr->num_data_prev+d] ;
+		
+		for (t = 0 ; t < curr->ones[d] ; ++t) {
+			pixel = curr->place_ones[curr->ones_accum[d] + t] ;
+			if (det->mask[pixel] < 1)
+				prob[r] += view[pixel] ;
+		}
+		
+		for (t = 0 ; t < curr->multi[d] ; ++t) {
+			pixel = curr->place_multi[curr->multi_accum[d] + t] ;
+			if (det->mask[pixel] < 1)
+				prob[r] += curr->count_multi[curr->multi_accum[d] + t] * view[pixel] ;
+		}
+		
+		if (prob[r] > max_exp) {
+			max_exp = prob[r] ;
+			priv->rmax[curr->num_data_prev+d] = r ;
+		}
+	}
+	
+	for (r = 0 ; r < num_rot_sub[d] ; ++r) {
+		prob[r] = exp(param->beta * (prob[r] - max_exp)) ;
+		p_sum += prob[r] ;
+	}
+	
+	for (r = 0 ; r < num_rot_sub[d] ; ++r) {
+		prob[r] /= p_sum ;
+		new_scale -= prob[r] * common->u[quat_sub[d][r]] ;
+		if (prob[r] < PROB_MIN)
+			continue ;
+		
+		memset(view, 0, det->num_pix * sizeof(double)) ;
+		
+		for (t = 0 ; t < curr->ones[d] ; ++t) {
+			pixel = curr->place_ones[curr->ones_accum[d] + t] ;
+			if (det->mask[pixel] < 2)
+				view[pixel] += prob[r] / iter->scale[curr->num_data_prev+d] ;
+		}
+		
+		for (t = 0 ; t < curr->multi[d] ; ++t) {
+			pixel = curr->place_multi[curr->multi_accum[d] + t] ;
+			if (det->mask[pixel] < 2)
+				view[pixel] += curr->count_multi[curr->multi_accum[d] + t] * prob[r] / iter->scale[curr->num_data_prev+d] ;
+		}
+		
+		(*slice_merge)(&quat->quat[quat_sub[d][r]*5], view, priv->model, priv->weight, iter->size, det) ;
+	}
+	
+	iter->scale[curr->num_data_prev + d] = frames->count[curr->num_data_prev + d] / new_scale ;
+	
+	if ((curr->num_data_prev+d)%(frames->tot_num_data/10) == 0)
+		fprintf(stderr, "\r\t\tFinished %d/%d frames", curr->num_data_prev+d, frames->tot_num_data) ;
+}
+*/
+
 void allocate_memory(struct max_data *data) {
 	int d, r ;
 	
 	data->rmax = calloc(frames->tot_num_data, sizeof(int)) ;
-	data->max_exp_p = malloc(frames->tot_num_data * sizeof(double)) ;
 	data->info = calloc(frames->tot_num_data, sizeof(double)) ;
 	data->likelihood = calloc(frames->tot_num_data, sizeof(double)) ;
-	for (d = 0 ; d < frames->tot_num_data ; ++d)
-		data->max_exp_p[d] = -DBL_MAX ;
+	if (!data->refinement) {
+		data->max_exp_p = malloc(frames->tot_num_data * sizeof(double)) ;
+		for (d = 0 ; d < frames->tot_num_data ; ++d)
+			data->max_exp_p[d] = -DBL_MAX ;
+	}
 	if (param->modes > 1)
 		data->quat_norm = calloc(param->modes, sizeof(double)) ;
 	
-	if (!data->within_openmp) {
-		data->probab = malloc(quat->num_rot_p * sizeof(double*)) ;
+	if (!data->within_openmp) { // common_data
 		data->u = calloc(quat->num_rot_p, sizeof(double)) ;
-		data->max_exp = calloc(frames->tot_num_data, sizeof(double)) ;
-		data->p_sum = calloc(frames->tot_num_data, sizeof(double)) ;
+		if (!data->refinement) { // Global search
+			data->probab = malloc(quat->num_rot_p * sizeof(double*)) ;
+			data->max_exp = calloc(frames->tot_num_data, sizeof(double)) ;
+			data->p_sum = calloc(frames->tot_num_data, sizeof(double)) ;
+			for (r = 0 ; r < quat->num_rot_p ; ++r)
+				data->probab[r] = malloc(frames->tot_num_data * sizeof(double)) ;
+		}
+		else {
+			data->probab = malloc(1 * sizeof(double*)) ;
+			//data->probab[0] = malloc(num_rot_sub_max * sizeof(double)) ;
+		}
 		
 		memset(iter->model2, 0, param->modes*iter->vol*sizeof(double)) ;
 		memset(iter->inter_weight, 0, param->modes*iter->vol*sizeof(double)) ;
-		for (r = 0 ; r < quat->num_rot_p ; ++r)
-			data->probab[r] = malloc(frames->tot_num_data * sizeof(double)) ;
 		print_max_time("alloc", "", param->rank == 0) ;
 	}
-	else {
-		data->p_sum = calloc(det[0].num_det, sizeof(double)) ;
-		data->view = malloc(det[0].num_det * sizeof(double*)) ;
+	else { // priv_data
+		data->all_views = malloc(det[0].num_det * sizeof(double*)) ;
 		for (d = 0 ; d < det[0].num_det ; ++d)
-			data->view[d] = malloc(det[d].num_pix * sizeof(double)) ;
+			data->all_views[d] = malloc(det[d].num_pix * sizeof(double)) ;
 		data->model = calloc(param->modes*iter->vol, sizeof(double)) ;
 		data->weight = calloc(param->modes*iter->vol, sizeof(double)) ;
 		if (param->need_scaling)
 			data->scale = calloc(frames->tot_num_data, sizeof(double)) ;
+		if (!data->refinement) // Global search
+			data->p_sum = calloc(det[0].num_det, sizeof(double)) ;
 	}
 }
 
@@ -171,7 +296,7 @@ double calculate_rescale(struct max_data *data) {
 void calculate_prob(int r, struct max_data *priv, struct max_data *common) {
 	int dset = 0, t, d, pixel, mode, rotind, detn, old_detn = -1 ;
 	struct dataset *curr = frames ;
-	double *prob = common->probab[r] ;
+	double *view, *prob = common->probab[r] ;
 	
 	rotind = (r*param->num_proc + param->rank) / param->modes ;
 	mode = (r*param->num_proc + param->rank) % param->modes ;
@@ -180,8 +305,9 @@ void calculate_prob(int r, struct max_data *priv, struct max_data *common) {
 	while (curr != NULL) {
 		//Calculate slice for current detector
 		detn = det[0].mapping[dset] ;
+		view = priv->all_views[detn] ;
 		if (detn != old_detn)
-			(*slice_gen)(&quat->quat[rotind*5], 1., priv->view[detn], &iter->model1[mode*iter->vol], iter->size, &det[detn]) ;
+			(*slice_gen)(&quat->quat[rotind*5], 1., view, &iter->model1[mode*iter->vol], iter->size, &det[detn]) ;
 		old_detn = detn ;
 		
 		// For each frame in data set
@@ -206,25 +332,25 @@ void calculate_prob(int r, struct max_data *priv, struct max_data *common) {
 				for (t = 0 ; t < curr->ones[d] ; ++t) {
 					pixel = curr->place_ones[curr->ones_accum[d] + t] ;
 					if (det[detn].mask[pixel] < 1)
-						prob[curr->num_data_prev+d] += priv->view[detn][pixel] ;
+						prob[curr->num_data_prev+d] += view[pixel] ;
 				}
 				
 				// For each pixel with count_multi photons
 				for (t = 0 ; t < curr->multi[d] ; ++t) {
 					pixel = curr->place_multi[curr->multi_accum[d] + t] ;
 					if (det[detn].mask[pixel] < 1)
-						prob[curr->num_data_prev+d] += curr->count_multi[curr->multi_accum[d] + t] * priv->view[detn][pixel] ;
+						prob[curr->num_data_prev+d] += curr->count_multi[curr->multi_accum[d] + t] * view[pixel] ;
 				}
 			}
 			else if (curr->type == 1) {
 				for (t = 0 ; t < det[detn].num_pix ; ++t)
 				if (det[detn].mask[t] < 1)
-					prob[curr->num_data_prev+d] += curr->int_frames[d*curr->num_pix + t] * priv->view[detn][t] ;
+					prob[curr->num_data_prev+d] += curr->int_frames[d*curr->num_pix + t] * view[t] ;
 			}
 			else if (curr->type == 2) { // Gaussian EMC for double precision data without scaling
 				for (t = 0 ; t < det[detn].num_pix ; ++t)
 				if (det[detn].mask[t] < 1)
-					prob[curr->num_data_prev+d] -= pow(curr->frames[d*curr->num_pix + t] - priv->view[detn][t]*iter->rescale, 2.) ;
+					prob[curr->num_data_prev+d] -= pow(curr->frames[d*curr->num_pix + t] - view[t]*iter->rescale, 2.) ;
 			}
 			
 			// Note maximum log-likelihood for each frame among 'r's tested by this MPI rank and OMP rank
@@ -297,7 +423,7 @@ void update_tomogram(int r, struct max_data *priv, struct max_data *common) {
 	int dset = 0, t, d, pixel, detn, rotind ;
 	double temp ;
 	struct dataset *curr ;
-	double *prob = common->probab[r] ;
+	double *view, *prob = common->probab[r] ;
 	
 	if (merge_frames != NULL) {
 		if (!param->rank && !r)
@@ -308,12 +434,13 @@ void update_tomogram(int r, struct max_data *priv, struct max_data *common) {
 		curr = frames ;
 	memset(priv->p_sum, 0, (det[0].num_det)*sizeof(double)) ;
 	for (detn = 0 ; detn < det[0].num_det ; ++detn) 
-		memset(priv->view[detn], 0, det[detn].num_pix*sizeof(double)) ;
+		memset(priv->all_views[detn], 0, det[detn].num_pix*sizeof(double)) ;
 	rotind = (r*param->num_proc + param->rank) / param->modes ;
 	
 	while (curr != NULL) {
 		//Calculate slice for current detector
 		detn = det[0].mapping[dset] ;
+		view = priv->all_views[detn] ;
 		
 		for (d = 0 ; d < curr->num_data ; ++d) {
 			// check if frame is blacklisted
@@ -356,24 +483,24 @@ void update_tomogram(int r, struct max_data *priv, struct max_data *common) {
 				for (t = 0 ; t < curr->ones[d] ; ++t) {
 					pixel = curr->place_ones[curr->ones_accum[d] + t] ;
 					if (det[detn].mask[pixel] < 2)
-						priv->view[detn][pixel] += prob[curr->num_data_prev+d] ;
+						view[pixel] += prob[curr->num_data_prev+d] ;
 				}
 				
 				// For all pixels with count_multi photons
 				for (t = 0 ; t < curr->multi[d] ; ++t) {
 					pixel = curr->place_multi[curr->multi_accum[d] + t] ;
 					if (det[detn].mask[pixel] < 2)
-						priv->view[detn][pixel] += curr->count_multi[curr->multi_accum[d] + t] * prob[curr->num_data_prev+d] ;
+						view[pixel] += curr->count_multi[curr->multi_accum[d] + t] * prob[curr->num_data_prev+d] ;
 				}
 			}
 			else if (curr->type == 1) {
 				for (t = 0 ; t < curr->num_pix ; ++t)
-					priv->view[detn][t] += curr->int_frames[d*curr->num_pix + t] * prob[curr->num_data_prev+d] ;
+					view[t] += curr->int_frames[d*curr->num_pix + t] * prob[curr->num_data_prev+d] ;
 			}
 			else if (curr->type == 2) { // Gaussian EMC update without scaling
 				for (t = 0 ; t < curr->num_pix ; ++t)
 				if (det[detn].mask[t] < 2)
-					priv->view[detn][t] += curr->frames[d*curr->num_pix + t] * prob[curr->num_data_prev+d] ;
+					view[t] += curr->frames[d*curr->num_pix + t] * prob[curr->num_data_prev+d] ;
 			}
 		}
 		
@@ -389,6 +516,7 @@ void update_tomogram(int r, struct max_data *priv, struct max_data *common) {
 
 void merge_tomogram(int r, struct max_data *priv) {
 	int detn, t, mode, rotind ;
+	double *view ;
 	
 	rotind = (r*param->num_proc + param->rank) / param->modes ;
 	mode = (r*param->num_proc + param->rank) % param->modes ;
@@ -396,11 +524,12 @@ void merge_tomogram(int r, struct max_data *priv) {
 	// If no data frame has any probability for this orientation, don't merge
 	// Otherwise divide the updated tomogram by the sum over all probabilities and merge
 	for (detn = 0 ; detn < det[0].num_det ; ++detn) {
+		view = priv->all_views[detn] ;
 		if (priv->p_sum[detn] > 0.) {
 			for (t = 0 ; t < det[detn].num_pix ; ++t)
-				priv->view[detn][t] /= priv->p_sum[detn] ;
+				view[t] /= priv->p_sum[detn] ;
 			
-			(*slice_merge)(&quat->quat[rotind*5], priv->view[detn], &priv->model[mode*iter->vol], &priv->weight[mode*iter->vol], iter->size, &det[detn]) ;
+			(*slice_merge)(&quat->quat[rotind*5], view, &priv->model[mode*iter->vol], &priv->weight[mode*iter->vol], iter->size, &det[detn]) ;
 		}
 	}
 	
@@ -534,8 +663,10 @@ void save_output(struct max_data *data) {
 }
 
 void free_memory(struct max_data *data) {
-	free(data->max_exp_p) ;
-	free(data->p_sum) ;
+	if (!data->refinement) {
+		free(data->max_exp_p) ;
+		free(data->p_sum) ;
+	}
 	free(data->info) ;
 	free(data->likelihood) ;
 	free(data->rmax) ;
@@ -543,15 +674,17 @@ void free_memory(struct max_data *data) {
 		free(data->quat_norm) ;
 	
 	if (!data->within_openmp) {
-		free(data->probab) ;
+		if (!data->refinement) {
+			free(data->probab) ;
+			free(data->max_exp) ;
+		}
 		free(data->u) ;
-		free(data->max_exp) ;
 	}
 	else {
 		int d ;
 		for (d = 0 ; d < det[0].num_det ; ++d)
-			free(data->view[d]) ;
-		free(data->view) ;
+			free(data->all_views[d]) ;
+		free(data->all_views) ;
 		if (param->need_scaling)
 			free(data->scale) ;
 		free(data->model) ;
