a01c22d31b4df85e181dc9d87711ad90e1871cfe
Kartik Ayyer <kartik.ayyer@desy.de>
2018-11-26 19:41:40 +0100

    emc: Fixed absolute beta_schedule implementation (beta calculated every iteration)

--- src/recon_emc.c
+++ src/recon_emc.c
@@ -121,9 +121,9 @@ void emc() {
 		MPI_Bcast(iter->model1, param->modes * iter->vol, MPI_DOUBLE, 0, MPI_COMM_WORLD) ;
 		
 		// Increasing beta by a factor of 'beta_jump' every 'beta_period' param->iterations
-		if (param->iteration % param->beta_period == 1 && param->iteration > 1)
-			//param->beta *= param->beta_jump ;
-			param->beta = param->beta_start * pow(param->beta_jump, param->iteration / param->beta_period) ;
+		//if (param->iteration % param->beta_period == 1 && param->iteration > 1)
+		//	param->beta *= param->beta_jump ;
+		param->beta = param->beta_start * pow(param->beta_jump, (param->iteration-1) / param->beta_period) ;
 		
 		likelihood = maximize() ;
 		print_recon_time("Completed maximize", &tr1, &tr2, param->rank) ;
