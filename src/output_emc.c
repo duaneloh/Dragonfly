c2efc8f1535daea07b51647d75d1cc918af4d54e
Kartik Ayyer <kartik.ayyer@desy.de>
2018-11-29 21:58:58 +0100

    output_emc: Saving all output in 1 H5 file per iteration if desired and compiled for

--- src/output_emc.c
+++ src/output_emc.c
@@ -24,9 +24,10 @@ void write_log_file_header(int num_threads) {
 	fprintf(fp, "\n\nIter\ttime\trms_change\tinfo_rate\tlog-likelihood\tnum_rot\tbeta\n") ;
 	fclose(fp) ;
 
+#ifdef WITH_HDF5
 	if (param->hdf5_out) {
 		char fname[2048] ;
-		hid_t file, dspace, dcpl ;
+		hid_t file, dset, dspace, dcpl ;
 		hsize_t init_size[1] = {0} ;
 		hsize_t max_size[1] = {H5S_UNLIMITED} ;
 		hsize_t chunk_size[1] = {1} ;
@@ -38,14 +39,26 @@ void write_log_file_header(int num_threads) {
 		dspace = H5Screate_simple(1, init_size, max_size) ;
 		dcpl = H5Pcreate(H5P_DATASET_CREATE) ;
 		H5Pset_chunk(dcpl, 1, chunk_size) ;
-		H5Dcreate(file, "/metrics/iter_time", H5T_IEEE_F64LE, dspace, H5P_DEFAULT, dcpl, H5P_DEFAULT) ;
-		H5Dcreate(file, "/metrics/rms_change", H5T_IEEE_F64LE, dspace, H5P_DEFAULT, dcpl, H5P_DEFAULT) ;
-		H5Dcreate(file, "/metrics/mutual_info", H5T_IEEE_F64LE, dspace, H5P_DEFAULT, dcpl, H5P_DEFAULT) ;
-		H5Dcreate(file, "/metrics/likelihood", H5T_IEEE_F64LE, dspace, H5P_DEFAULT, dcpl, H5P_DEFAULT) ;
-		H5Dcreate(file, "/params/num_rot", H5T_STD_I32LE, dspace, H5P_DEFAULT, dcpl, H5P_DEFAULT) ;
-		H5Dcreate(file, "/params/beta", H5T_IEEE_F64LE, dspace, H5P_DEFAULT, dcpl, H5P_DEFAULT) ;
+		dset = H5Dcreate(file, "/metrics/iter_time", H5T_IEEE_F64LE, dspace, H5P_DEFAULT, dcpl, H5P_DEFAULT) ;
+		H5Dclose(dset) ;
+		dset = H5Dcreate(file, "/metrics/rms_change", H5T_IEEE_F64LE, dspace, H5P_DEFAULT, dcpl, H5P_DEFAULT) ;
+		H5Dclose(dset) ;
+		dset = H5Dcreate(file, "/metrics/mutual_info", H5T_IEEE_F64LE, dspace, H5P_DEFAULT, dcpl, H5P_DEFAULT) ;
+		H5Dclose(dset) ;
+		dset = H5Dcreate(file, "/metrics/likelihood", H5T_IEEE_F64LE, dspace, H5P_DEFAULT, dcpl, H5P_DEFAULT) ;
+		H5Dclose(dset) ;
+		dset = H5Dcreate(file, "/params/num_rot", H5T_STD_I32LE, dspace, H5P_DEFAULT, dcpl, H5P_DEFAULT) ;
+		H5Dclose(dset) ;
+		dset = H5Dcreate(file, "/params/beta", H5T_IEEE_F64LE, dspace, H5P_DEFAULT, dcpl, H5P_DEFAULT) ;
+		H5Dclose(dset) ;
+		H5Fclose(file) ;
+		
+		sprintf(fname, "%s/output.h5", param->output_folder) ;
+		file = H5Fcreate(fname, H5F_ACC_TRUNC, H5P_DEFAULT, H5P_DEFAULT) ;
+		H5Gcreate(file, "iterations", H5P_DEFAULT, H5P_DEFAULT, H5P_DEFAULT) ;
 		H5Fclose(file) ;
 	}
+#endif //WITH_HDF5
 }
 
 void update_log_file(double iter_time, double likelihood) {
@@ -55,6 +68,7 @@ void update_log_file(double iter_time, double likelihood) {
 	fprintf(fp, "%1.4e\t%f\t%.6e\t%-7d\t%f\n", iter->rms_change, iter->mutual_info, likelihood, quat->num_rot, param->beta) ;
 	fclose(fp) ;
 
+#ifdef WITH_HDF5
 	if (param->hdf5_out) {
 		hid_t file, dset, dspace, wspace ;
 		char fname[2048] ;
@@ -71,29 +85,36 @@ void update_log_file(double iter_time, double likelihood) {
 		H5Sselect_hyperslab(dspace, H5S_SELECT_SET, sel_start, NULL, sel_count, NULL) ;
 		wspace = H5Screate_simple(1, sel_count, NULL) ;
 		H5Dwrite(dset, H5T_IEEE_F64LE, wspace, dspace, H5P_DEFAULT, &iter_time) ;
+		H5Dclose(dset) ;
 		
 		dset = H5Dopen(file, "/metrics/rms_change", H5P_DEFAULT) ;
 		H5Dset_extent(dset, new_size) ;
 		H5Dwrite(dset, H5T_IEEE_F64LE, wspace, dspace, H5P_DEFAULT, &(iter->rms_change)) ;
+		H5Dclose(dset) ;
 		
 		dset = H5Dopen(file, "/metrics/mutual_info", H5P_DEFAULT) ;
 		H5Dset_extent(dset, new_size) ;
 		H5Dwrite(dset, H5T_IEEE_F64LE, wspace, dspace, H5P_DEFAULT, &(iter->mutual_info)) ;
+		H5Dclose(dset) ;
 		
 		dset = H5Dopen(file, "/metrics/likelihood", H5P_DEFAULT) ;
 		H5Dset_extent(dset, new_size) ;
 		H5Dwrite(dset, H5T_IEEE_F64LE, wspace, dspace, H5P_DEFAULT, &likelihood) ;
+		H5Dclose(dset) ;
 		
 		dset = H5Dopen(file, "/params/num_rot", H5P_DEFAULT) ;
 		H5Dset_extent(dset, new_size) ;
 		H5Dwrite(dset, H5T_STD_I32LE, wspace, dspace, H5P_DEFAULT, &(quat->num_rot)) ;
+		H5Dclose(dset) ;
 		
 		dset = H5Dopen(file, "/params/beta", H5P_DEFAULT) ;
 		H5Dset_extent(dset, new_size) ;
 		H5Dwrite(dset, H5T_IEEE_F64LE, wspace, dspace, H5P_DEFAULT, &(param->beta)) ;
+		H5Dclose(dset) ;
 		
 		H5Fclose(file) ;
 	}
+#endif //WITH_HDF5
 }
 
 void save_models() {
@@ -109,33 +130,38 @@ void save_models() {
 	fp = fopen(fname, "w") ;
 	fwrite(iter->inter_weight, sizeof(double), param->modes * iter->vol, fp) ;
 	fclose(fp) ;
+	
+#ifdef WITH_HDF5
+	if (param->hdf5_out) {
+		hid_t file, dset, dspace ;
+		char name[2048] ;
+		hsize_t out_size[4] ;
+		out_size[0] = param->modes ;
+		out_size[1] = iter->size ;
+		out_size[2] = iter->size ;
+		out_size[3] = iter->size ;
+		
+		sprintf(name, "%s/output_%.3d.h5", param->output_folder, param->iteration) ;
+		file = H5Fopen(name, H5F_ACC_RDWR, H5P_DEFAULT) ;
+		
+		dspace = H5Screate_simple(4, out_size, NULL) ;
+		dset = H5Dcreate(file, "/intens", H5T_IEEE_F64LE, dspace, H5P_DEFAULT, H5P_DEFAULT, H5P_DEFAULT) ;
+		H5Dwrite(dset, H5T_IEEE_F64LE, H5S_ALL, dspace, H5P_DEFAULT, iter->model1) ;
+		H5Dclose(dset) ;
+		
+		dspace = H5Screate_simple(4, out_size, NULL) ;
+		dset = H5Dcreate(file, "/inter_weight", H5T_IEEE_F64LE, dspace, H5P_DEFAULT, H5P_DEFAULT, H5P_DEFAULT) ;
+		H5Dwrite(dset, H5T_IEEE_F64LE, H5S_ALL, dspace, H5P_DEFAULT, iter->inter_weight) ;
+		H5Dclose(dset) ;
+		
+		H5Fclose(file) ;
+	}
+#endif //WITH_HDF5
 }
 
 void save_metrics(struct max_data *data) {
 	int d ;
 	
-	// Write scale factors to file even when not updating them
-	if (param->need_scaling) {	
-		char fname[2048] ;
-		sprintf(fname, "%s/scale/scale_%.3d.dat", param->output_folder, param->iteration) ;
-		FILE *fp_scale = fopen(fname, "w") ;
-		for (d = 0 ; d < frames->tot_num_data ; ++d)
-			fprintf(fp_scale, "%.15e\n", iter->scale[d]) ;
-		fclose(fp_scale) ;
-	}
-	
-	/*
-	if (param->modes > 1) {
-		int r ;
-		fprintf(stderr, "Mode occupancies: ") ;
-		for (r = 0 ; r < param->modes ; ++r)
-			fprintf(stderr, "%.3f ", data->quat_norm[r]/(frames->tot_num_data - frames->num_blacklist)) ;
-		fprintf(stderr, "\n") ;
-		for (r = 0 ; r < quat->num_rot ; ++r)
-			quat->quat[r*5 + 4] = data->quat_norm[r/param->rot_per_mode] / (frames->tot_num_data - frames->num_blacklist) ;
-	}
-	*/
-
 	// Print frame-by-frame mutual information, likelihood, and most likely orientations to file
 	char fname[2048] ;
 	sprintf(fname, "%s/mutualInfo/info_%.3d.dat", param->output_folder, param->iteration) ;
@@ -155,6 +181,16 @@ void save_metrics(struct max_data *data) {
 	fclose(fp_info) ;
 	fclose(fp_likelihood) ;
 	
+	// Write scale factors to file even when not updating them
+	if (param->need_scaling) {	
+		char fname[2048] ;
+		sprintf(fname, "%s/scale/scale_%.3d.dat", param->output_folder, param->iteration) ;
+		FILE *fp_scale = fopen(fname, "w") ;
+		for (d = 0 ; d < frames->tot_num_data ; ++d)
+			fprintf(fp_scale, "%.15e\n", iter->scale[d]) ;
+		fclose(fp_scale) ;
+	}
+	
 	// Write frame-by-frame mode occupancies to file
 	if (param->modes > 1) {
 		sprintf(fname, "%s/modes/occupancies_%.3d.bin", param->output_folder, param->iteration) ;
@@ -162,5 +198,46 @@ void save_metrics(struct max_data *data) {
 		fwrite(data->quat_norm, sizeof(double), frames->tot_num_data*param->modes, fp_modes) ;
 		fclose(fp_modes) ;
 	}
+	
+#ifdef WITH_HDF5
+	if (param->hdf5_out) {
+		char name[2048] ;
+		hid_t file, dset, dspace ;
+		hsize_t len[1] ;
+		
+		len[0] = frames->tot_num_data ;
+		sprintf(name, "%s/output_%.3d.h5", param->output_folder, param->iteration) ;
+		file = H5Fcreate(name, H5F_ACC_TRUNC, H5P_DEFAULT, H5P_DEFAULT) ;
+		
+		dspace = H5Screate_simple(1, len, NULL) ;
+		dset = H5Dcreate(file, "orientations", H5T_STD_I32LE, dspace, H5P_DEFAULT, H5P_DEFAULT, H5P_DEFAULT) ;
+		H5Dwrite(dset, H5T_STD_I32LE, H5S_ALL, dspace, H5P_DEFAULT, data->rmax) ;
+		H5Dclose(dset) ;
+		
+		dset = H5Dcreate(file, "mutual_info", H5T_STD_I32LE, dspace, H5P_DEFAULT, H5P_DEFAULT, H5P_DEFAULT) ;
+		H5Dwrite(dset, H5T_IEEE_F64LE, H5S_ALL, dspace, H5P_DEFAULT, data->info) ;
+		H5Dclose(dset) ;
+		
+		dset = H5Dcreate(file, "likelihood", H5T_STD_I32LE, dspace, H5P_DEFAULT, H5P_DEFAULT, H5P_DEFAULT) ;
+		H5Dwrite(dset, H5T_IEEE_F64LE, H5S_ALL, dspace, H5P_DEFAULT, data->likelihood) ;
+		H5Dclose(dset) ;
+		
+		if (param->need_scaling) {
+			dset = H5Dcreate(file, "scale", H5T_STD_I32LE, dspace, H5P_DEFAULT, H5P_DEFAULT, H5P_DEFAULT) ;
+			H5Dwrite(dset, H5T_IEEE_F64LE, H5S_ALL, dspace, H5P_DEFAULT, iter->scale) ;
+			H5Dclose(dset) ;
+		}
+		
+		if (param->modes > 1) {
+			len[0] = param->modes * frames->tot_num_data ;
+			dspace = H5Screate_simple(1, len, NULL) ;
+			dset = H5Dcreate(file, "occupancies", H5T_STD_I32LE, dspace, H5P_DEFAULT, H5P_DEFAULT, H5P_DEFAULT) ;
+			H5Dwrite(dset, H5T_IEEE_F64LE, H5S_ALL, dspace, H5P_DEFAULT, data->quat_norm) ;
+			H5Dclose(dset) ;
+		}
+		
+		H5Fclose(file) ;
+	}
+#endif //WITH_HDF5
 }
 
