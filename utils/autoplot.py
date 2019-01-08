640067ad4b94a71b6c0b0737781515c83889cd56
Kartik Ayyer <kartik.ayyer@desy.de>
2019-01-08 17:20:19 +0100

    autoplot: Rearranged menubar. Also slightly split beta_change and num_rot_change lines

--- utils/autoplot.py
+++ utils/autoplot.py
@@ -262,9 +262,9 @@ class LogPlotter(object):
         subp.set_ylim(ylim)
         subp.xaxis.set_major_locator(matplotlib.ticker.MaxNLocator(integer=True))
         for i in self.beta_change:
-            subp.plot([i+1, i+1], ylim, 'w--', lw=1)
+            subp.plot([i+1-0.1, i+1-0.1], ylim, 'w--', lw=1)
         for i in self.num_rot_change[:-1]:
-            subp.plot([i+1, i+1], ylim, 'r--', color='tab:orange', lw=1)
+            subp.plot([i+1+0.1, i+1+0.1], ylim, 'r--', color='tab:orange', lw=1)
 
 class ProgressViewer(QtWidgets.QMainWindow):
     '''GUI to track progress of EMC reconstruction
@@ -320,18 +320,21 @@ class ProgressViewer(QtWidgets.QMainWindow):
         action = QtWidgets.QAction('&Load Volume', self)
         action.triggered.connect(self._load_volume)
         filemenu.addAction(action)
-        action = QtWidgets.QAction('&Save Image', self)
-        action.triggered.connect(self._save_plot)
-        filemenu.addAction(action)
-        action = QtWidgets.QAction('Save Log &Plot', self)
-        action.triggered.connect(self._save_log_plot)
-        filemenu.addAction(action)
         action = QtWidgets.QAction('&Quit', self)
         action.triggered.connect(self.close)
         filemenu.addAction(action)
 
-        # Color map picker
-        cmapmenu = menubar.addMenu('&Color Map')
+        # Image Menu
+        imagemenu = menubar.addMenu('&Image')
+        action = QtWidgets.QAction('&Save Slices Image', self)
+        action.triggered.connect(self._save_plot)
+        imagemenu.addAction(action)
+        action = QtWidgets.QAction('Save Log &Plot', self)
+        action.triggered.connect(self._save_log_plot)
+        imagemenu.addAction(action)
+        
+        # -- Color map picker
+        cmapmenu = imagemenu.addMenu('&Color Map')
         self.color_map = QtWidgets.QActionGroup(self, exclusive=True)
         for i, cmap in enumerate(['coolwarm', 'cubehelix', 'CMRmap', 'gray', 'gray_r', 'jet']):
             action = self.color_map.addAction(QtWidgets.QAction(cmap, self, checkable=True))
