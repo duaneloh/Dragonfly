e2722b8f63f6f53875005d30791295f620a6012b
Kartik Ayyer <kartik.ayyer@desy.de>
2019-02-14 06:22:23 -0800

    psanatodet: Satisfied linter

--- utils/convert/psanatodet.py
+++ utils/convert/psanatodet.py
@@ -12,15 +12,14 @@ Produces:
 '''
 
 import os
-import numpy as np
-import h5py
 import sys
 import logging
+import numpy as np
 #Add utils directory to pythonpath
 sys.path.append(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
-from py_src import py_utils
-from py_src import read_config
-from py_src import detector
+from py_src import py_utils # pylint: disable=wrong-import-position
+from py_src import read_config # pylint: disable=wrong-import-position
+from py_src import detector # pylint: disable=wrong-import-position
 
 try:
     import psana
@@ -28,34 +27,40 @@ except ImportError:
     sys.stderr.write('This utility needs PSANA to run. Unable to import psana module\n')
     sys.exit(1)
 
-if __name__ == '__main__':
-    logging.basicConfig(filename='recon.log', level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+def main():
+    """Parse command line arguments and generate file"""
+    logging.basicConfig(filename='recon.log', level=logging.INFO,
+                        format='%(asctime)s - %(levelname)s - %(message)s')
     parser = py_utils.MyArgparser(description='PSANA to det')
     parser.add_argument('exp_string', help='PSANA experiment string')
-    parser.add_argument('det_name', help='Detector name (either source string or alias)')
-    parser.add_argument('-C', '--corners', help='Set corner pixels to be irrelevant. Default = False', action='store_true', default=False)
+    parser.add_argument('det_name', help='PSANA Detector name (either source string or alias)')
+    parser.add_argument('-C', '--corners',
+                        help='Set corner pixels to be irrelevant. Default = False',
+                        action='store_true', default=False)
     args = parser.special_parse_args()
 
     logging.info('Starting psanatodet...')
     logging.info(' '.join(sys.argv))
-    pm = read_config.get_detector_config(args.config_file, show=args.vb)
-    q_pm = read_config.compute_q_params(pm['detd'], pm['dets_x'], pm['dets_y'], pm['pixsize'], pm['wavelength'], pm['ewald_rad'], show=args.vb)
+    pm = read_config.get_detector_config(args.config_file, show=args.vb) # pylint: disable=invalid-name
+    q_pm = read_config.compute_q_params(pm['detd'], pm['dets_x'], pm['dets_y'],
+                                        pm['pixsize'], pm['wavelength'],
+                                        pm['ewald_rad'], show=args.vb)
     output_folder = read_config.get_filename(args.config_file, 'emc', 'output_folder')
 
-    ds = psana.DataSource(args.exp_string + ':idx')
-    run = ds.runs().next()
+    dsource = psana.DataSource(args.exp_string + ':idx')
+    run = dsource.runs().next()
     times = run.times()
     evt = run.event(times[0])
     psana_det = psana.Detector(args.det_name)
-    cx = psana_det.coords_x(evt).flatten() * 1.e-3 # in mm
-    cy = psana_det.coords_y(evt).flatten() * 1.e-3 # in mm
+    cx = psana_det.coords_x(evt).flatten() * 1.e-3 # pylint: disable=invalid-name
+    cy = psana_det.coords_y(evt).flatten() * 1.e-3 # pylint: disable=invalid-name
 
     detd = pm['detd'] # in mm
     qscaling = 1. / pm['wavelength'] / q_pm['q_sep']
     rad = np.sqrt(cx*cx + cy*cy)
     norm = np.sqrt(cx*cx + cy*cy + detd*detd)
     polar = read_config.compute_polarization(pm['polarization'], cx, cy, norm)
-    
+
     det = detector.Detector()
     det.detd = detd / pm['pixsize']
     det.ewald_rad = pm['ewald_rad']
@@ -66,11 +71,12 @@ if __name__ == '__main__':
     det.corr *= polar
 
     if pm['mask_fname'] is None:
-        det.raw_mask = psana_det.mask(evt, status=True, calib=True, edges=True, central=True, unbondnbrs8=True).flatten()
+        det.raw_mask = psana_det.mask(evt, status=True, calib=True, edges=True,
+                                      central=True, unbondnbrs8=True).flatten()
         det.raw_mask = 2*(1 - det.raw_mask)
         if args.corners:
             rmax = min(cx.max(), np.abs(cx.min()), cy.max(), np.abs(cy.min()))
-            det.raw_mask[(rad>rmax) & (mask==0)] = 1
+            det.raw_mask[(rad > rmax) & (det.raw_mask == 0)] = 1
     else:
         det.raw_mask = np.fromfile(pm['mask_fname'], '=u1')
 
@@ -82,3 +88,6 @@ if __name__ == '__main__':
         det_file += '.dat'
     sys.stderr.write('Writing detector file to %s\n' % det_file)
     det.write(det_file)
+
+if __name__ == '__main__':
+    main()
