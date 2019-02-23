77ac9fb73db5c661b5e76b63976be1be04a489ff
Kartik Ayyer <kartik.ayyer@desy.de>
2019-02-23 13:18:02 +0800

    run_tests: Also generating HDF5 versions of detector and photons files

--- utils/run_tests.sh
+++ utils/run_tests.sh
@@ -18,6 +18,14 @@ cd ${test_dir}
 cp ../sample_configs/testing.ini config.ini
 ./utils/sim_setup.py -y --skip_data
 ./utils/make_data -T -t 4
+sed -i "s/det_sim.dat/det_sim.h5/" config.ini
+./utils/make_detector.py
+sed -i "s/det_sim.h5/det_sim.dat/" config.ini
+sed -i "s/photons.emc/photons.h5/" config.ini
+./utils/make_data -T -t 4
+sed -i "s/photons.h5/photons.emc/" config.ini
+echo
+echo
 
 cd ${root_dir}/pyemc
 python -V
