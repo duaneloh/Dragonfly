6f46836abb960cf780e179bae57087a3e9fe8f83
Kartik Ayyer <kartik.ayyer@desy.de>
2019-06-04 14:17:57 +0200

    readvol: Added ability to parse h5 output

--- utils/py_src/readvol.py
+++ utils/py_src/readvol.py
@@ -1,14 +1,23 @@
-'''Module containing EMCReader class to parse 2D stack .bin files'''
+'''Module containing EMCReader class to parse 2D stack files'''
 
 from __future__ import print_function
 import sys
 import numpy as np
+try:
+    import h5py
+    HDF5_MODE = True
+except ImportError:
+    HSD5_MODE = False
 
 class VolReader(object):
     def __init__(self, stack_fname, size):
         self.fname = stack_fname
         self.size = size
-        self.stack = np.fromfile(stack_fname).reshape(-1, size, size)
+        if HDF5_MODE and h5py.is_hdf5(stack_fname):
+            with h5py.File(stack_fname, 'r') as f:
+                self.stack = f['intens'][:]
+        else:
+            self.stack = np.fromfile(stack_fname).reshape(-1, size, size)
         
         self.flist = [{'fname': stack_fname,
             'num_data': self.stack.shape[0],
