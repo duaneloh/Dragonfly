aa5ec6179f7622e26a4b1b1b13ae64c9419a9b5f
Kartik Ayyer <kartik.ayyer@desy.de>
2019-10-04 17:32:34 +0200

    setup: Included HDF5 compile and link args

--- setup.py
+++ setup.py
@@ -3,21 +3,26 @@ from setuptools.extension import Extension
 from Cython.Distutils import build_ext
 from Cython.Build import cythonize
 
+import subprocess
 import numpy as np
+import h5py
+out = subprocess.getoutput('h5cc -shlib -show')
+hdf5_cflags = [[s for s in out.split() if s[:2] == '-I'][0]]
+hdf5_libs = [[s for s in out.split() if s[:2] == '-L'][0], '-lhdf5']
 
 include_dirs = [np.get_include()]
 #compile_args = '-fopenmp -O3 -Wall -Wno-cpp -Wno-unused-result -Wno-unused-function -Wno-format-overflow -DFIXED_SEED'.split()
-compile_args = '-fopenmp -O3 -Wall -Wno-cpp -Wno-unused-result -Wno-unused-function -Wno-format-overflow'.split()
-link_args = '-lm -fopenmp'.split()
+compile_args = '-fopenmp -O3 -Wall -Wno-cpp -Wno-unused-result -Wno-unused-function -Wno-format-overflow'.split() + hdf5_cflags
+link_args = '-lm -fopenmp'.split() + hdf5_libs
 ext_modules = [
     Extension(name='dragonfly.detector', sources=['dragonfly/detector.pyx'],
         depends=['dragonfly/detector.h', 'dragonfly/detector.pxd'],
         language='c', extra_compile_args=compile_args, extra_link_args=link_args),
-    Extension(name='dragonfly.emcfile', sources=['dragonfly/emcfile.pyx'],
-        depends=['dragonfly/emcfile.pxd'],
+    Extension(name='dragonfly.emcfile', sources=['dragonfly/emcfile.pyx', 'dragonfly/src/emcfile.c'],
+        depends=['dragonfly/emcfile.pxd', 'dragonfly/src/emcfile.h'],
         language='c', extra_compile_args=compile_args, extra_link_args=link_args),
     Extension(name='dragonfly.model', sources=['dragonfly/model.pyx', 'dragonfly/src/model.c'],
-        depends=['dragonfly/model.h', 'dragonfly/model.pxd'],
+        depends=['dragonfly/src/model.h', 'dragonfly/model.pxd'],
         language='c', extra_compile_args=compile_args, extra_link_args=link_args),
     Extension(name='dragonfly.quaternion', sources=['dragonfly/quaternion.pyx', 'dragonfly/src/quat.c'],
         depends=['dragonfly/src/quat.h', 'dragonfly/quaternion.pxd'], include_dirs=include_dirs,
