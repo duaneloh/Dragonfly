2440d7184c004135f316cdf69baf475587cbb096
Kartik Ayyer <kartik.ayyer@desy.de>
2019-10-14 15:21:12 +0200

    params: Added from_config() method

--- dragonfly/params.pyx
+++ dragonfly/params.pyx
@@ -1,3 +1,6 @@
+import os.path as op
+from configparser import ConfigParser
+
 from libc.stdlib cimport malloc, calloc, free
 from libc.string cimport strcpy
 from . cimport params as c_params
@@ -6,35 +9,39 @@ from .params cimport EMCParams, params
 cdef class EMCParams:
     def __init__(self):
         self.par = <c_params.params*> calloc(1, sizeof(c_params.params))
+
         self.par.rank = 0
         self.par.num_proc = 1
-        self.par.known_scale = 0
         self.par.start_iter = 1
-        self.par.beta_period = 100
-        self.par.beta_jump = 1.
-        self.par.beta_factor = -1.
-        self.par.radius = 0.
-        self.par.radius_period = 100
-        self.par.radius_jump = 0.
-        self.par.oversampling = 10.
-        self.par.need_scaling = 0
-        self.par.update_scale = 1
-        self.par.alpha = 0.
-        self.par.sigmasq = 0.
-        self.par.num_modes = 1
-        self.par.nonrot_modes = 0
-        self.par.rot_per_mode = 0
-        self.par.rtype = c_params.RECON3D
-        self.par.friedel_sym = 0
-        self.par.save_prob = 0
-        self.par.refine = 0
-        self.par.coarse_div = 0
-        self.par.fine_div = 0
-        # TODO: Add config file path when using config file
+        self.par.known_scale = 0
+
         self.par.log_fname = <char*> malloc(1024)
         strcpy(self.par.log_fname, b'EMC.log')
         self.par.output_folder= <char*> malloc(1024)
         strcpy(self.par.output_folder , b'data/')
+        self.par.rtype = c_params.RECON3D
+
+        self.par.need_scaling = 0
+        self.par.alpha = 0.
+        self.par.beta_factor = -1.
+        self.par.radius = 0.
+        self.par.num_modes = 1
+        self.par.nonrot_modes = 0
+        self.par.rot_per_mode = 0
+
+        self.par.beta_jump = 1.
+        self.par.beta_period = 100
+        self.par.radius_jump = 0.
+        self.par.radius_period = 100
+
+        self.par.oversampling = 10.
+        self.par.sigmasq = 0.
+        self.par.friedel_sym = 0
+        self.par.save_prob = 0
+        self.par.update_scale = 1
+        self.par.refine = 0
+        self.par.coarse_div = 0
+        self.par.fine_div = 0
 
     def free(self):
         if self.par == NULL:
@@ -46,6 +53,50 @@ cdef class EMCParams:
             free(self.par.output_folder)
         free(self.par)
         self.par = NULL
+
+    def from_config(self, config_fname, section_name='emc'):
+        config_folder = op.dirname(config_fname).encode()
+        config = ConfigParser()
+        config.read(config_fname)
+
+        strcpy(self.par.output_folder, op.join(config_folder, config.get(section_name, 'output_folder', fallback='data/').encode()))
+        strcpy(self.par.log_fname, op.join(config_folder, config.get(section_name, 'log_file', fallback='EMC.log').encode()))
+        rtype = config.get(section_name, 'recon_type', fallback='3d')
+        if rtype == '3d':
+            self.par.rtype = c_params.RECON3D
+        elif rtype == '2d':
+            self.par.rtype = c_params.RECON2D
+        elif rtype == 'rz':
+            self.par.rtype = c_params.RECONRZ
+        else:
+            raise ValueError('Unknown recon_type %s'%rtype)
+        
+        self.par.need_scaling = config.getint(section_name, 'need_scaling', fallback=0)
+        self.par.alpha = config.getfloat(section_name, 'alpha', fallback=0.)
+        self.par.beta_factor = config.getfloat(section_name, 'beta_factor', fallback=-1.)
+        self.par.radius = config.getfloat(section_name, 'radius', fallback=0.)
+        self.par.num_modes = config.getint(section_name, 'num_modes', fallback=1)
+        self.par.nonrot_modes = config.getint(section_name, 'num_nonrot_modes', fallback=0)
+        self.par.rot_per_mode = config.getint(section_name, 'num_rot', fallback=0)
+
+        beta_schedule = tuple(config.get(section_name, 'beta_schedule', fallback='1. 100').split())
+        self.par.beta_jump = float(beta_schedule[0])
+        self.par.beta_period = int(beta_schedule[1])
+        radius_schedule= tuple(config.get(section_name, 'radius_schedule', fallback='0. 100').split())
+        self.par.radius_jump = float(radius_schedule[0])
+        self.par.radius_period = int(radius_schedule[1])
+
+        self.par.oversampling = config.getfloat(section_name, 'oversampling', fallback=10.)
+        self.par.sigmasq = config.getfloat(section_name, 'gaussian_sigma', fallback=0.)**2
+        self.par.friedel_sym = config.getint(section_name, 'friedel_sym', fallback=0)
+        self.par.save_prob = config.getint(section_name, 'save_prob', fallback=0)
+        self.par.update_scale = config.getint(section_name, 'update_scale', fallback=1)
+        num_divs = config.get(section_name, 'num_div').split()
+        if len(num_divs) > 1:
+            self.par.refine = 1
+            self.par.fine_div = int(num_divs[0])
+            self.par.coarse_div = int(num_divs[1])
+            print("Doing refinement from num_div = %d -> %d\n" % (self.par.coarse_div, self.par.fine_div))
 
     @property
     def rank(self): return self.par.rank
