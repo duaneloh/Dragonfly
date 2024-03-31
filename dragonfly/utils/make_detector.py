d566ba3dcfedca4a948341aeb91825e2550d745c
Kartik Ayyer <kartik.ayyer@desy.de>
2024-03-31 08:43:24 +0200

    d:make_detector: Moved to dragonfly folder and using detector functions

---
+++ dragonfly/utils/make_detector.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python
+
+'''Module to generate detector file from configuration parameters'''
+
+import sys
+import logging
+import argparse
+
+import numpy as np
+import dragonfly
+
+from py_src import read_config
+from py_src import py_utils
+
+def make_detector(config_fname, yes=False, verbose=False):
+    '''Generate detector file from parameters in config file'''
+    config = read_config.MyConfigParser()
+    config.read(config_fname)
+
+    det_fname = config.get_filename('make_detector', 'out_detector_file')
+
+    if yes or py_utils.check_to_overwrite(det_fname):
+        timer = py_utils.MyTimer()
+        pm = config.get_detector_config(show=verbose)
+        read_config.compute_q_params(pm['detd'], pm['dets_x'],
+                                     pm['dets_y'], pm['pixsize'],
+                                     pm['wavelength'], pm['ewald_rad'], show=verbose)
+        timer.reset('Reading experiment parameters', report=verbose)
+
+        det = dragonfly.Detector()
+        x, y = np.meshgrid(np.arange(pm['dets_x'])-pm['detc_x'],
+                           np.arange(pm['dets_y'])-pm['detc_y'],
+                           indexing='ij')
+        det.cx = x.flatten()
+        det.cy = y.flatten()
+        det.detd = pm['detd'] / pm['pixsize']
+        det.ewald_rad = pm['ewald_rad']
+        det.calc_from_coords(pol=pm['polarization'])
+
+        rad = np.sqrt(det.cx**2 + det.cy**2)
+        if pm['mask_fname'] is None:
+            det.raw_mask = np.zeros(det.corr.shape, dtype='u1')
+            det.raw_mask[rad > min(pm['detc_x'], pm['detc_y'])] = 1
+            det.raw_mask[rad < pm['stoprad']] = 2
+        else:
+            det.raw_mask = np.fromfile(pm['mask_fname'], '=u1')
+            det.raw_mask[(rad > min(pm['detc_x'], pm['detc_y'])) & (det.raw_mask == 0)] = 1
+        timer.reset('Creating detector', report=verbose)
+
+        det.write(det_fname)
+        timer.reset('Writing detector to %s' % det_fname, report=verbose)
+
+        timer.report_time_since_beginning()
+
+def main():
+    '''Parses command line arguments and config file to generate detector file'''
+    parser = argparse.ArgumentParser(description='Make detector file')
+    parser.add_argument('-c', '--config_fname', default='config.ini',
+                        help='Path to config file (Default: config.ini)')
+    parser.add_argument('-v', '--verbose', action='store_true', help='Verbose logging')
+    parser.add_argument('-y', '--yes', action='store_true',
+                        help='Say yes to all prompts')
+    args = parser.parse_args()
+
+    logging.basicConfig(filename='simdata.log', level=logging.INFO,
+                        format='%(asctime)s - %(levelname)s - %(message)s')
+    logging.info('\n\nStarting make_detector....')
+    logging.info(' '.join(sys.argv))
+
+    make_detector(args.config_fname, yes=args.yes, verbose=args.verbose)
+
+if __name__ == '__main__':
+    main()
