30e6399d46585c3cbd779c6201a2eee6fe708050
Kartik Ayyer <kartik.ayyer@desy.de>
2024-04-01 22:33:31 +0200

    d:make_data: Added argparse and main()

--- dragonfly/utils/make_data.pyx
+++ dragonfly/utils/make_data.pyx
@@ -1,12 +1,15 @@
 import sys
 import os.path as op
+import time
+import argparse
+import logging
 
 import numpy as np
 import dragonfly
 from cython.parallel import parallel, prange
 cimport openmp
 
-from .py_src import read_config
+from .py_src import read_config, py_utils
 
 def rand_quat():
     while True:
@@ -16,16 +19,20 @@ def rand_quat():
             break
     return qvals / qnorm
 
-def main():
+def make_data(config_fname, yes=False, verbose=False):
     config = read_config.MyConfigParser()
-    config.read('config.ini')
-    
+    config.read(config_fname)
+
     num_data = config.getint('make_data', 'num_data')
     fluence = config.getfloat('make_data', 'fluence')
     intens_fname = config.get_filename('make_data', 'in_intensity_file')
     det_fname = config.get_filename('make_data', 'in_detector_file')
     out_fname = config.get_filename('make_data', 'out_photons_file')
-    
+
+    if not (yes or py_utils.check_to_overwrite(out_fname)):
+        return
+
+    timer = py_utils.MyTimer()
     model = dragonfly.Model(1)
     try:
         model.allocate(intens_fname)
@@ -34,7 +41,7 @@ def main():
         model.free()
         model = dragonfly.Model(size)
         model.allocate(intens_fname)
-    
+
     det = dragonfly.CDetector(det_fname, norm=False)
 
     rescale = fluence * 2.81794e-9**2
@@ -43,7 +50,9 @@ def main():
     hdf5_output = True
     if op.splitext(out_fname)[1] == '.emc':
         hdf5_output = False
+    timer.reset('Ready to generate data', report=verbose)
 
+    stime = time.time()
     wemc = dragonfly.EMCWriter(out_fname, det.num_pix, hdf5=hdf5_output)
     view = np.zeros(det.num_pix, dtype='f8')
     #print(openmp.omp_get_max_threads())
@@ -54,8 +63,30 @@ def main():
         wemc.write_frame(phot.ravel())
         sys.stderr.write('\r%d/%d'%(i+1, num_data))
     sys.stderr.write('\n')
+    timer.reset('Generated frames', report=verbose)
 
     wemc.finish_write()
+    print('Time taken: %.3f s' % (time.time()-timer._time_start))
+    timer.reset('Writing to file', report=verbose)
+
+    timer.report_time_since_beginning()
+
+def main():
+    '''Parse command line arguments and generate electron density volume with config file'''
+    parser = argparse.ArgumentParser(description='Make data file from 3D intensities')
+    parser.add_argument('-c', '--config_fname', default='config.ini',
+                        help='Path to config file (Default: config.ini)')
+    parser.add_argument('-v', '--verbose', action='store_true', help='Verbose logging')
+    parser.add_argument('-y', '--yes', action='store_true',
+                        help='Say yes to all prompts')
+    args = parser.parse_args()
+
+    logging.basicConfig(filename='simdata.log', level=logging.INFO,
+                        format='%(asctime)s - %(levelname)s - %(message)s')
+    logging.info('\n\nStarting.... make_data')
+    logging.info(' '.join(sys.argv))
+
+    make_data(args.config_fname, yes=args.yes, verbose=args.verbose)
 
 if __name__ == '__main__':
     main()
