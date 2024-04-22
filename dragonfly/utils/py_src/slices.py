7a6774fb65b4e399b156d94702340a382dd7c65e
Kartik Ayyer <kartik.ayyer@desy.de>
2024-04-22 19:30:26 +0200

    d:slices: Takes config file as input and can run with 2d and 3d recons

--- dragonfly/utils/py_src/slices.py
+++ dragonfly/utils/py_src/slices.py
@@ -1,101 +1,83 @@
 '''Module containing class to generate tomographic slices'''
 
 import os
-import numpy as np
+
 import h5py
+import dragonfly
+
+from .read_config import MyConfigParser
 
 class SliceGenerator(object):
     '''Class to generate slices from 3D intensity distribution for given orientation
-    
-    Requires:
-        Detector object - Instance of Detector
-        Quaternion filename - Typically made using utils/make_quaternion
-        Output folder [Optional] - Path to data folder where reconstruction results are stored
-    
+
+    Requires config file from an EMC reconstruction
+
     Methods:
         get_slice(iteration, frame_number) - Return predicted detector intensity for iteration'th\
             reconstruction for given frame
 
     NOTE: This produces the tomogram for the most likely orientation only.
     '''
-    def __init__(self, geom, quat_fname, folder='data/', need_scaling=False):
-        self.geom = geom
-        self.quat = np.loadtxt(quat_fname, skiprows=1, usecols=(0, 1, 2, 3))
-        self.folder = folder
-        self.current_iteration = -1
-        self.need_scaling = need_scaling
+    def __init__(self, config_fname):
+        config = MyConfigParser()
+        config.read(config_fname)
 
+        self.det = dragonfly.Detector(config.get_filename('emc', 'in_detector_file'))
+        self.recon_type = config.get('emc', 'recon_type', fallback='3d').lower()
+        if self.recon_type == '2d':
+            num_rot = config.getint('emc', 'num_rot')
+            friedel_sym = config.getboolean('emc', 'friedel_sym', fallback=False)
+            point_group = '2' if friedel_sym else '1'
+            self.quat = dragonfly.Quaternion(num_rot=num_rot, point_group=point_group)
+        elif self.recon_type == '3d':
+            num_div = config.getint('emc', 'num_div')
+            self.quat = dragonfly.Quaternion(num_div)
+        self.folder = config.get_filename('emc', 'output_folder', fallback='data/')
+        self.need_scaling = config.getboolean('emc', 'need_scaling', fallback=False)
+
+        self.current_iteration = -1
         self.model = self.stats = None
 
     def _init_model(self, iteration):
         self.stats = {'rmax': None, 'scale': None, 'info': None}
 
-        model_fname = '%s/output/intens_%.3d.bin' % (self.folder, iteration)
         h5model_fname = '%s/output_%.3d.h5' % (self.folder, iteration)
-        if os.path.isfile(model_fname):
-            print('Parsing comparison model:', model_fname)
-            self.model = np.fromfile(model_fname, '=f8')
-            size = int(np.round(self.model.shape[0]**(1./3.)))
-            self.model = self.model.reshape(3 * (size,))
-
-            self.stats['rmax'] = np.fromfile('%s/orientations/orientations_%.3d.bin' %
-                                             (self.folder, iteration), '=i4')
+        print('Parsing comparison output:', h5model_fname)
+        with h5py.File(h5model_fname, 'r') as fptr:
+            size = fptr['intens'].shape[-1]
+            self.num_modes = fptr['intens'].shape[0]
+            self.stats['rmax'] = fptr['orientations'][:]
+            self.stats['info'] = fptr['mutual_info'][:]
             if self.need_scaling:
-                try:
-                    self.stats['scale'] = np.loadtxt('%s/scale/scale_%.3d.dat' %
-                                                     (self.folder, iteration))
-                except IOError:
-                    self.stats['scale'] = np.ones(self.stats['rmax'].shape)
-            else:
-                self.stats['scale'] = np.ones(self.stats['rmax'].shape)
-            self.stats['info'] = np.loadtxt('%s/mutualInfo/info_%.3d.dat' % (self.folder, iteration))
-        elif os.path.isfile(h5model_fname):
-            print('Parsing comparison output:', h5model_fname)
-            with h5py.File(h5model_fname, 'r') as fptr:
-                self.model = fptr['intens'][0]
-                self.stats['rmax'] = fptr['orientations'][:]
-                self.stats['info'] = fptr['mutual_info'][:]
-                if self.need_scaling:
-                    self.stats['scale'] = fptr['scale'][:]
+                self.stats['scale'] = fptr['scale'][:]
+        self.model = dragonfly.Model(size, self.num_modes, model_type=self.recon_type)
+        self.model.allocate(h5model_fname)
 
         self.current_iteration = iteration
 
-    def _gen_rot_matrix(self, num):
-        qw, qx, qy, qz = tuple(self.quat[num]) # pylint: disable=C0103
-        rot = np.zeros((3, 3))
-        rot[0] = [1-2*qy*qy-2*qz*qz, 2*qx*qy - 2*qz*qw, 2*qx*qz + 2*qy*qw]
-        rot[1] = [2*qx*qy + 2*qz*qw, 1-2*qx*qx-2*qz*qz, 2*qy*qz - 2*qx*qw]
-        rot[2] = [2*qx*qz - 2*qy*qw, 2*qy*qz + 2*qx*qw, 1-2*qx*qx-2*qy*qy]
-        return np.linalg.inv(rot)
-
     def get_slice(self, iteration, num, raw=False):
         '''Get tomographic slice for given iteration and frame number
-        
+
         Parameters:
             iteration - Reconstruction iteration number
             num - Frame number
             raw [optional] - Whether to return unassembled slice
-        
+
         Returns:
             Assembled detector slice, mutual information for that iteration
         '''
         if iteration != self.current_iteration:
             self._init_model(iteration)
-        rot_matrix = self._gen_rot_matrix(self.stats['rmax'][num])
-
-        size = self.model.shape[0]
-        coords = self.geom.qvals.T
-        rot_coords = np.dot(rot_matrix, coords)
-        rot_coords = np.round((rot_coords + size/2)).astype('i4')
-        rot_coords[(rot_coords < 0) | (rot_coords > size-1)] = 0
-        dslice = self.model[rot_coords[0], rot_coords[1], rot_coords[2]]
+        mode_num = self.stats['rmax'][num] % self.num_modes
+        quat_num = self.stats['rmax'][num] // self.num_modes
+        dslice = self.model.slice_gen(self.quat.quats[quat_num], self.det, mode=mode_num)
 
         if raw:
             return dslice, self.stats['info'][num]
-        return self.geom.assemble_frame(dslice), self.stats['info'][num]
+        return self.det.assemble_frame(dslice), self.stats['info'][num]
 
     def get_quat(self, iteration, num):
         '''Return best match quaternion for given iteration and frame number'''
         if iteration != self.current_iteration:
             self._init_model(iteration)
-        return tuple(self.quat[self.stats['rmax'][num]])
+        return tuple(self.quat.quats[self.stats['rmax'][num] // self.num_modes])
